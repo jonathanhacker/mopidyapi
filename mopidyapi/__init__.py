18af4a5f74e240cd5261829c36ee4f6e9d20992a
jonathanhacker <github@jhacker.de>
2020-04-17 20:21:34 +0200

    Add `MopidyAPI` to `__all__`
    When importing `MopidyAPI` as shown in the readme, PyCharm complains about it not being defined in `__all__`. This can easily be resolved by adding it. Alternatively, I would suggest changing the import in the Readme to `from mopidyapi.client import MopidyAPI
    `, which works as well without warning.

--- mopidyapi/__init__.py
+++ mopidyapi/__init__.py
@@ -5,7 +5,7 @@ MopidyAPI is a Python (3.6+) library for interacting with Mopidy,
 using its JSON RPC API.
 """
 
-__all__ = ['client', 'wsclient', 'exceptions']
+__all__ = ['MopidyAPI', 'client', 'wsclient', 'exceptions']
 __version__ = '1.0.0'
 
 from .client import MopidyAPI
